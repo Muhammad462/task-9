0a3377fd5f46702397ad3dc977d0d4cf4d3e2171
Muhammad462 <139973126+Muhammad462@users.noreply.github.com>
2023-07-25 15:57:46 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+//task 9:Adding comment
+//program 8:This program prints my favorite number
+//program 4:This program store the famous name with its quote
+var favoritenumber = "333";
+console.log(`my favorite number${favoritenumber}`);
+var author = "Albert Einstein";
+var quote = "i never teach my pupils i only provide the conditions in which they could learn";
